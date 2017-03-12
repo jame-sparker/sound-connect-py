76d2903cd0bfabba5b3f0373ec717d982f8853d2
James Parker <jsparker512@gmail.com>
2017-03-12 14:07:23 +1100

    Basic client/host code

--- client.py
+++ client.py
@@ -3,3 +3,12 @@
     Date: 12th March 2017
 """
 
+import socket
+
+s = socket.socket()
+host = socket.gethostname()
+port = 54321
+
+s.connect((host, port))
+print s.recv(1024)
+s.close
--- host.py
+++ host.py
@@ -2,3 +2,29 @@
     Author: James Parker
     Date: 12th March 2017
 """
+
+import socket
+
+class Host:
+
+    def setup_socket(self):
+        self.s = socket.socket()
+        host = socket.gethostname()
+        port = 54321
+        self.s.bind((host, port))
+
+
+    def listen(self):
+
+        self.s.listen(5)
+
+        while True:
+            c, addr = self.s.accept()
+            print 'Got connection from', addr
+            c.send('Thank you for connecting')
+            c.close()
+
+
+host = Host()
+host.setup_socket()
+host.listen()
